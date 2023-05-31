3f85de69b15a8ffe9368c2e7d6e084b89d0730fd
kimdoha <kkhhjj3393@gmail.com>
2023-05-31 14:25:15 +0900

    feat: typescript, router setting

---
+++ src/pages/home-page.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const HomePage = () => {
+
+    return (
+        <header>
+            <h1>AI FACE MUSIC</h1>
+            <div>
+                <a href = "#">Home</a>
+                <a href = "#">Main</a>
+            </div>
+        </header>
+    )
+};
+
+export default HomePage;
---
+++ src/pages/main-page.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const MainPage = () => {
+    return (
+        <header></header>
+    )
+};
+
+export default MainPage;
