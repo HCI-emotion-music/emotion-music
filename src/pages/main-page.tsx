86f6c0b86d11d0017a85133fdcc79e860f19fe13
kimdoha <kkhhjj3393@gmail.com>
2023-06-01 14:06:18 +0900

    feat: create app-main-submit-button & photo-upload

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,46 +1,23 @@
-import { PhotoIcon } from "@heroicons/react/24/solid";
 import AppMenu from "../components/app-menu";
+import AppMainTitle from "../components/app-main-title";
+import AppMainPhotoUpload from "../components/app-main-photo-upload";
+import AppMainSubmitButton from "../components/app-main-submit-button";
 
 const MainPage = () => {
   return (
     <div>
       <AppMenu />
-      <form>
-        <div>
-          <h1>FACE&TEXT</h1>
-        </div>
-        {/* PHOTO */}
-        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-          <div className="text-center">
-            <PhotoIcon
-              className="mx-auto h-12 text-gray-300"
-              area-hidden="true"
-            />
-            <div className="mt-4 flex text-sm leading-6 text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600"
-              >
-                <span>Upload a file</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
-                />
-              </label>
-              <p className="pl-1"> or drag and drop</p>
-            </div>
-            <p className="text-xs leading-5 text-gray-600">
-              PNG, JPG, GIF up to 10MB
-            </p>
-          </div>
-        </div>
+      <AppMainTitle />
+
+      <div className="px-20">
+        <AppMainPhotoUpload />
+
+
         {/* TEXT */}
-        <div>
+        <div className="py-10">
           <label
             htmlFor=""
-            className="block text-sm font-medium leading-6 text-gray-900"
+            className="block text-xl/2 font-medium leading-6 text-gray-900"
           >
             Text
           </label>
@@ -50,12 +27,16 @@ const MainPage = () => {
               name="text"
               id="text"
               className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="입력해주세요."
+              placeholder="오늘 당신의 감정을 적어주세요."
             />
           </div>
         </div>
-      </form>
+
+        <AppMainSubmitButton />
+      </div>
     </div>
+
+    
 
   );
 };
