75ce710393c8528f22b2934b52bfda23236c1304
kimdoha <kkhhjj3393@gmail.com>
2023-06-01 12:04:35 +0900

    feat: create app-home-title component

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import AppMenu from "../components/app-menu";
+import { PlayCircleIcon } from "@heroicons/react/24/solid";
+import AppHomeTitle from "../components/app-home-title";
 
 const HomePage = () => {
   return (
     <div>
         <AppMenu />
 
-        {/* TITLE */}
-        <div className="bg-blue-100">
-            <h2 className="text-6xl font-bold tracking-tight text-white">AI FACE MUSIC</h2>
-            <p className="text-white">Click to Play</p>
-            <button
-                type="button"
-                className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 hover:bg-gray-50">
-                아이콘 집어 넣기
-            </button>
+        <div className="flex pt-16 lg:pt-32 pb-24 lg:pb-52 overflow-hidden bg-black">
+            <AppHomeTitle />
+            <div className="w-full md:w-1/2 p-8 flex items-center justify-center py-5">
+                <div className="flex items-center justify-content">
+                    <p className="text-white text-1xl md:text-1xl p-0">Click Here to Play</p>
+                    <PlayCircleIcon
+                        className="mx-auto h-12 text-gray-200 p-2" 
+                        area-hidden="true">
+                    </PlayCircleIcon>
+
+                </div>
+            </div>
         </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-import defaultTheme from "tailwindcss/defaultTheme";
-
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
