a5f784a594cd110e6d07f8fede48602f532c2d2a
youngseonseo <sue000731@naver.com>
2023-06-13 16:00:36 +0900

    modify result page css

--- src/pages/result-page.js
+++ src/pages/result-page.js
@@ -4,6 +4,7 @@ import cdLogo from '../image/cd.png';
 import playLogo from '../image/play.png';
 import  { positiveList, negativeList } from "../api/spotify";
 
+
 function App() {
   return (
     <div className="App">
@@ -14,11 +15,15 @@ function App() {
       <header className="App-header">
         <div className="head">YOUR MUSIC</div>
         <div class="parent">
-          <img src={cdLogo} alt="cdImage"/>
-          <img src={playLogo} alt="playImage" class="overlay-image"/>
+          <div className='putImage'>
+            <img id="cd" src={cdLogo} alt="cdImage"/>
+            <a href='/playMusic'>
+              <img id="play" src={playLogo} alt="playImage" class="overlay-image"/>
+            </a>
+          </div>
         </div>
         <div className="RecommendText">
-          <h6>TITLE : KITSCH</h6>
+          <h6>TITLE : Dance Monkey</h6>
           <h6>RECOMMEND : DANCE</h6>
         </div>
       </header>
